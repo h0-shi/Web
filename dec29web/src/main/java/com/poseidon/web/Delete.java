8570c5086fd9a01a47a36480ff874ce355dbd83a
h0-shi <whdgus9655@naver.com>
2023-12-29 17:46:06 +0900

    23 12 29 수업 끝

--- dec29web/src/main/java/com/poseidon/web/Delete.java
+++ dec29web/src/main/java/com/poseidon/web/Delete.java
@@ -13,25 +13,26 @@ import com.poseidon.dao.BoardDAO;
 @WebServlet("/delete")
 public class Delete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public Delete() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Delete() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
 		String no = request.getParameter("no");
-		
+
 		BoardDAO dao = new BoardDAO();
 		dao.delete(no);
-		
+
 		response.sendRedirect("./index.jsp");
-	
+
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 //		doGet(request, response);
-	
-	
 	}
 
 }
